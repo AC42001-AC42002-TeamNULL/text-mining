2063e02c8f352a3fdef031a94f709a1612afcae5
Anthony Grimes <contact@anthonygrimes.co.uk>
2018-03-19 13:10:21 +0000

    Text only extraction

--- wk3.py
+++ wk3.py
@@ -1,12 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 
 def get_review_texts(review_html):
     """Get all the reviews on a review page"""
     soup = BeautifulSoup(review_html, 'lxml')
-    block = soup.find_all(True, "span", class_="review-text")
-    return block
+    block = soup.find_all("span", class_="review-text")
+    cleanr = re.compile('<.*?>')
+
+    reviews = []
+
+    for blk in range(0, len(block)):
+        reviews.append(re.sub(cleanr, '', str(block[blk])))
+
+    return reviews
 
 
 def get_all_reviews(review_url):
@@ -20,7 +28,7 @@ def get_all_reviews(review_url):
 url = 'https://www.amazon.com/Play-Doh-Modeling-Compound-Exclusive-Non-Toxic/product-reviews/B00JM5GW10/?pageNumber='
 
 reviews = []
-for i in range(1, 20):
+for i in range(1, 30):
     url = url + str(i)
     reviews.extend(get_all_reviews(url))
 

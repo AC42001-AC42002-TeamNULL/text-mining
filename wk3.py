026d5f0c7f07c971d1e2bcc431d03168c75c2dfe
Anthony Grimes <contact@anthonygrimes.co.uk>
2018-03-23 17:29:16 +0000

    Wk3 argument extraction with rules and eval

--- wk3.py
+++ wk3.py
@@ -1,16 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-from nltk import pos_tag, word_tokenize, RegexpParser
-import nltk
-from nltk.sem import relextract
+from nltk import pos_tag, word_tokenize
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk.sentiment import SentimentAnalyzer
-from nltk.probability import FreqDist
-from itertools import chain
-from nltk.classify import NaiveBayesClassifier, accuracy
-import collections
-from nltk.metrics.scores import precision, f_measure, recall
+
 
 def get_review_texts(review_html):
     soup = BeautifulSoup(review_html, 'lxml')
@@ -32,6 +26,8 @@ def get_all_reviews(review_url):
     return reviews
 
 
+## Retrieve reviews from amazon
+
 url = 'https://www.amazon.com/Play-Doh-Modeling-Compound-Exclusive-Non-Toxic/product-reviews/B00JM5GW10/?pageNumber='
 
 reviews = []
@@ -39,40 +35,62 @@ for i in range(1, 2):
     url = url + str(i)
     reviews.extend(get_all_reviews(url))
 
-
+# Create sentiment analyser
 sentim_analyzer = SentimentAnalyzer()
 sid = SentimentIntensityAnalyzer()
 
+segements = []
+segementsPol = []
+rule = 'VBD'
+
+# POS tag our reviews by words
 for review in reviews:
     tokens = word_tokenize(review)
     pos_tags = pos_tag(tokens)
 
-    segements = []
-    segementsPol = []
-
+    # Extract and split segments based on VBD tagging
     for tag in pos_tags:
-        if tag[1] == 'VBD':  # split based on verb, past tense
+        if tag[1] == rule:  # split based on verb, past tense
             segements.extend(review.split(tag[0]))
 
-    for segment in segements:
-        ss = sid.polarity_scores(segment)
+# Determine the segment polarity using VADER
+for segment in segements:
+    ss = sid.polarity_scores(segment)
 
-        label = ''
+    label = ''
 
-        if ss['compound'] >= .2:
-            label = 'pos'
+    if ss['compound'] >= .2:
+        label = 'pos'
 
-        if ss['compound'] <= -.2:
-            label = 'neg'
+    if ss['compound'] <= -.2:
+        label = 'neg'
 
-        if -.2 < ss['compound'] < .2:
-            label = 'neu'
+    if -.2 < ss['compound'] < .2:
+        label = 'neu'
 
-        segementsPol.append([segment, label])
+    segementsPol.append([segment, label])
 
-    print(segementsPol)
+p_pos = None
+c_pos = None
+c_guess = 0
+i_guess = 0
+# For each segment try to see if there's an indicator for the proceeding segments based on rule VBD markers
+# The idea being that if you split an argument into two the connecting discourse marker in this case the verb will
+# split an argument into premise/prefix and explanation/suffix and so will have matching polarities
+for seg, pos in segementsPol:
+    if p_pos is None:
+        p_pos = pos
 
-    # print(pos_tags)
+    c_pos = pos
+
+    if c_pos == p_pos:
+        c_guess += 1
+    i_guess += 1
+
+print('Using ' + rule + '. We correctly matched argument segments in ' + str(c_guess) + ' out of ' + str(
+    i_guess) + ' segments')
+
+# print(pos_tags)
 
 # for review in reviews:
 #     tok = word_tokenize(review)
